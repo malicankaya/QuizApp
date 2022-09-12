44b0d03ffd83f200bf61f5ab61d414adcbd83704
malicankaya <malicankaya97@gmail.com>
2022-09-12 17:56:58 +0300

    setting up the question ui p2

--- app/src/main/java/com/malicankaya/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/malicankaya/quizapp/QuizQuestionsActivity.kt
@@ -1,16 +1,60 @@
 package com.malicankaya.quizapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionsActivity : AppCompatActivity() {
+
+    var tvQuestion: TextView? = null
+    var ivFlag: ImageView? = null
+    var progressBar: ProgressBar? = null
+    var tvProgressBar: TextView? = null
+
+    var tvOptionOne: TextView? = null
+    var tvOptionTwo: TextView? = null
+    var tvOptionThree: TextView? = null
+    var tvOptionFour: TextView? = null
+
+    var btnSubmit: Button? = null
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-        val questions = Constants.getQuestions()
+        tvQuestion = findViewById(R.id.tvQuestion)
+        ivFlag = findViewById(R.id.ivFlag)
+        progressBar = findViewById(R.id.progressBar)
+        tvProgressBar = findViewById(R.id.tvProgressBar)
 
-        Log.i("QuestionsList size is","${questions.size}")
+        tvOptionOne = findViewById(R.id.tvOptionOne)
+        tvOptionTwo = findViewById(R.id.tvOptionTwo)
+        tvOptionThree = findViewById(R.id.tvOptionThree)
+        tvOptionFour = findViewById(R.id.tvOptionFour)
+
+        btnSubmit = findViewById(R.id.btnSubmit)
+        val questionsList = Constants.getQuestions()
+        val questionsListSize = questionsList.size
+        var currentPosition = 1
+
+        progressBar?.max = questionsListSize
+
+
+        for (i in questionsList){
+            tvQuestion?.text = i.question
+            ivFlag?.setImageResource(i.image)
+            progressBar?.progress = currentPosition
+            tvProgressBar?.text = "$currentPosition / $questionsListSize"
+
+            tvOptionOne?.text = i.optionOne
+            tvOptionTwo?.text = i.optionTwo
+            tvOptionThree?.text = i.optionThree
+            tvOptionFour?.text = i.optionFour
+        }
     }
 }
